8f59ea827c007ac8d7b022c9d460fac5c6a1204c
Ryuu <ishiryuuu17@gmail.com>
2022-05-02 19:15:00 +0900

    first commit

---
+++ BullCowGame/README.md
@@ -0,0 +1,3 @@
+1. Guess the Isogram(a word with no repeating letters)
+2. Letter in guess and right place is a “Bull”.
+3. Letter in guess, but wrong place is a “Cow”.
---
+++ Shooters/Source/Shooters/ShootersGameModeBase.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+
+#include "ShootersGameModeBase.h"
+
+void AShootersGameModeBase::PawnKilled(APawn* PawnKilled) 
+{
+    
+}
